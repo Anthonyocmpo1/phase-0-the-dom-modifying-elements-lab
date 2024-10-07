8fb17cf76a7b5a2de7ca4172a7520978ef1697b0
Anthony mwaura <antony.mwaura@student.moringaschool.comc0deanthony@anthony-HP-EliteBook-840-G5:~/f11$ git config --global user.email antony.mwaura@student.moringaschool.com>
2024-10-07 14:08:24 +0300

    the-dom-modifying-element

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+console.log("Script is running");
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Your Header Text";
+newHeader.textContent = "Your name is the champion"
+document.body.appendChild(newHeader);
+console.log(newHeader);
